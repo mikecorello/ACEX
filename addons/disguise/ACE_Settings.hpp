fb6d9f10216415c034f41fd1a234114d4d588b9f
Mike Corello <mike@corello10.net>
2016-10-16 19:28:44 +0100

    Add ACE_Settings for each class

--- addons/disguise/ACE_Settings.hpp
+++ addons/disguise/ACE_Settings.hpp
@@ -4,4 +4,39 @@ class ACE_Settings {
         typeName = "BOOL";
         displayName = CSTRING(Enable);
     };
+    class GVAR(ignoreNVG) {
+        value = 0;
+        typeName = "BOOL";
+        displayName = CSTRING(IgnoreNVG);
+    };
+    class GVAR(headgear) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Headgear);
+    };
+    class GVAR(goggles) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Goggles);
+    };
+    class GVAR(uniforms) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Uniforms);
+    };
+    class GVAR(vests) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Vests);
+    };
+    class GVAR(backpacks) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Backpacks);
+    };
+    class GVAR(vehicles) {
+        value = "";
+        typeName = "STRING";
+        displayName = CSTRING(Vehicles);
+    };
 };
